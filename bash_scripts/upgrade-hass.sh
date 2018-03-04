93956ebfe1f8265b1b84822620ec889c359bdfa2
Valiceemo <richiewallace83@gmail.com>
2018-03-04 02:00:02 +0000

    Daily commit Sun  4 Mar 02:00:01 GMT 2018

--- bash_scripts/upgrade-hass.sh
+++ bash_scripts/upgrade-hass.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-###########################################
-## This script upgrades the OS on my pi, ##
-## upgrades HA then reboots the device   ##
-## having pulled any config changes from ##
-## Github if needed.                     ##
-###########################################
 
-cd /home/homeassistant/.homeassistant/
-sudo su -s /bin/bash homeassistant
-source /srv/homeassistant/bin/activate
+#cd /home/homeassistant/.homeassistant/
+#sudo su -s /bin/bash homeassistant
+#source /srv/homeassistant/bin/activate
 #pip3 install --upgrade homeassistant
+
+
+# Become user 'hass'
+sudo su -s /bin/bash homeassistant <<'EOF'
+# Move to home assistant dir
+cd /home/homeassistant/.homeassistant
+# Activate the virtualenv
+source /srv/homeassistant/bin/activate
+
+# Install Home Assistant
+#pip3 install --upgrade homeassistant exit
+EOF
